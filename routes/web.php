0e4ec25caa336bd32d61fc74a59594934eacb5ae
amoragues <amoragues@iesmanacor.cat>
2023-02-01 12:22:24 +0000

    afegir productes controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ProductesController;
+ 
+Route::get('/productes', [ProductesController::class, 'index']);
